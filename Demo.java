b38c709d59189502bcd33f9c41ede1b91bb5170b
naresh6685 <knaresh6685@gmail.com>
2023-02-25 12:29:39 +0530

    dsdddds rdrrwe sddfd sd

--- Demo.java
+++ Demo.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo{
  public static void main(String[] args){
   System.out.println("kjdjkdfkjdffffddsdsjk");
   System.out.println("Git push");
-   
+  
+  List<String> list = new ArrayList<>();
+  list.add("kjskjds");
+  list.add("kjskjds");
+  list.add("kjskjds");
+  list.add("kjskjds");
+  list.add("kjskjds");
+  
  }
   
 }
